92cca7c5f00f685981441be9f80bf5c8a2195dcd
Joaquin Giorgis <joa12386@gmail.com>
2025-08-03 17:14:40 -0300

    first step

---
+++ reverseVowels.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function (s) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+    const arr = s.split('')
+    let left = 0
+    let right = arr.length - 1
+
+    while (left < right) {
+
+    }
+
+    return arr
+};
---
+++ summaryRanges.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+
+var summaryRanges = function (nums) {
+    const result = [];
+
+    if (nums.length === 0) return result;
+
+    let start = nums[0]
+
+    for (let i = 1; i <= nums.length; i++) {
+
+        if (i === nums.length || nums[i] !== nums[i - 1] + 1) {
+            if (start === nums[i - 1]) {
+                result.push(`${start}`)
+            } else {
+                result.push(`${start}->${nums[i - 1]}`)
+            }
+            start = nums[i]
+        }
+
+    }
+
+    return result;
+};
